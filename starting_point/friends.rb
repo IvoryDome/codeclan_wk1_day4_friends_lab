2da850c6bab8aed1f20b2c4554e0d81d58efbdad
elipinska <ewa.lipinska@hotmail.com>
2018-03-08 14:50:30 +0000

    Completed exercises

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -1,3 +1,50 @@
 def get_name(person)
   return person[:name]
 end
+
+def favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, food)
+  return person[:favourites][:things_to_eat].include?(food)
+end
+
+def add_friend(person, friend)
+  person[:friends].push(friend)
+end
+
+def unfriend(person, friend)
+  person[:friends].delete(friend)
+end
+
+def total_money(array_of_people)
+  total_money = 0
+  for person in array_of_people
+    total_money += person[:monies]
+  end
+  return total_money
+end
+
+def loan_of_money(lender, lendee, money_loaned)
+  lender[:monies] -= money_loaned
+  lendee[:monies] += money_loaned
+end
+
+def everyones_favourite_food(array_of_people)
+  favourite_foods = []
+  for person in array_of_people
+    favourite_foods.concat(person[:favourites][:things_to_eat])
+  end
+  return favourite_foods
+end
+
+def no_friends(array_of_people)
+  array_of_no_friends = []
+  for person in array_of_people
+    if person[:friends] == []
+      array_of_no_friends.push(person[:name])
+    end
+  end
+  return array_of_no_friends
+end
--- starting_point/specs/friends_spec.rb
+++ starting_point/specs/friends_spec.rb
@@ -72,34 +72,69 @@ class TestFriends < MiniTest::Test
 
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
+  def test_favourite_tv_show
+    result = favourite_tv_show(@person2)
+    assert_equal("Baywatch", result)
+  end
 
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person3, "stew") should return true, likes_to_eat(@person3, "spinach") should return false)
+  def test_likes_to_eat
+    result = likes_to_eat(@person3, "stew")
+    assert_equal(true, result)
+  end
 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Darren") should add Darren to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
+  def test_add_friend
+    add_friend(@person2, "Darren")
+    result = @person2[:friends].length
+    assert_equal(2, result)
+  end
 
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
+  def test_unfriend
+    unfriend(@person4, "Jay")
+    result = @person4[:friends].length
+    assert_equal(2, result)
+  end
 
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
-
+  def test_total_money
+      result = total_money(@people)
+      assert_equal(143, result)
+  end
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
   # (hint2: You should test if both the lender's and the lendee's money have changed, maybe two assertions?)
-
+def test_loan_of_money
+    loan_of_money(@person5, @person1, 50)
+    lenders_money = @person5[:monies]
+    lendees_money = @person1[:monies]
+    assert_equal(50, lenders_money)
+    assert_equal(51, lendees_money)
+end
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/things_to_eat arrays together)
-
+def test_everyones_favourite_food
+  result = everyones_favourite_food(@people)
+  assert_equal(["charcuterie", "soup", "bread", "ratatouille", "stew", "spaghetti", "spinach"], result)
+end
 
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
+def test_no_friends
+  result = no_friends(@people)
+  assert_equal(["Dave"], result)
+end
+
 
 end
